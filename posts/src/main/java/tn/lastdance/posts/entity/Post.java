1accf9ca54a4ed37904aa503d6277c82824f1bd0
abdou6666 <abdousfayhi12@gmail.com>
2023-10-20 22:00:58 +0100

    finished basic crud

--- posts/src/main/java/tn/lastdance/posts/entity/Post.java
+++ posts/src/main/java/tn/lastdance/posts/entity/Post.java
@@ -1,29 +1,27 @@
 package tn.lastdance.posts.entity;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 //@Data
-@Builder
-@Document(collation = "posts")
+//@Builder
+//@Document(collation = "posts")
 //@JsonInclude(JsonInclude.Include.NON_NULL)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "posts")
+@Builder
 public class Post {
     @Id
     private String postId;
     private String username;
     private String title;
     private String content;
-    public Post(){
-        super();
-    }
 
-    public Post(String postId, String username, String title, String content) {
-        this.postId = postId;
-        this.username = username;
-        this.title = title;
-        this.content = content;
-    }
 }
