e28806c9d6e00980521d1f239b491b4f1ac7cefe
dalijp147 <medali.bouderbala@esprit.tn>
2023-10-21 14:18:44 +0100

    add cours microservice with cud using h2 and eureka Client

---
+++ cours/src/test/java/tn/lastdance/cours/CoursApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.lastdance.cours;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
