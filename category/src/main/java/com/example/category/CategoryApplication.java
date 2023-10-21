d3ee66fdaf573d8b8f57af87adecf7b4d3d832eb
BAAAWB <92890853+IhebBaoueb@users.noreply.github.com>
2023-10-22 00:53:08 +0100

    eureka_Docker

--- category/src/main/java/com/example/category/CategoryApplication.java
+++ category/src/main/java/com/example/category/CategoryApplication.java
@@ -6,24 +6,18 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import java.util.stream.Stream;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CategoryApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CategoryApplication.class, args);
     }
 
-    @Bean
-    ApplicationRunner init(CategoryRepository repository) {
-        return args -> {
-            Stream.of("Mariem", "Sarra", "Mohamed").forEach(nom -> {
-                repository.save(new Category(nom));
-            });
-            repository.findAll().forEach(System.out::println);
-        };
-    }
+
 
 }
